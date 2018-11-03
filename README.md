2f77f8744ef2d4137516862b05aa6276739f11c2
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-11-03 19:32:16 +0100

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# ShowHintsAlsoInOtherMasters
-Glyphs.app plug-in for displaying hints from the hint-origin master also in other masters
+# Show Hints Also in Other Masters
+
+Glyphs.app plug-in for displaying hints from the hint-origin master (which is usually the first master unless you set the custom parameter `Get Hints from Master` in *File > Font Info > Font*) also in other masters. The main idea is to find stem hint overlaps that could cause Hint Replacement. Thanks to Jeremy Tankard for the idea.
+
+![ShowHintsAlsoInOtherMasters](ShowHintsAlsoInOtherMasters.png)
+
+It also displays in the hints in the hint-origin master if *View > Show Hints* is turned off.
+
+### Installation
+
+1. One-click install *Show Hints Also in Other Masters* from *Window > Plugin Manager*
+2. Restart Glyphs.
+
+### Usage Instructions
+
+1. Open at least one glyph in Edit View.
+2. Use *View > Show Hints Also in Other Masters* to toggle the display of the hints.
+
+### Requirements
+
+The plug-in works in Glyphs 2.5.2 in High Sierra. I can only test it in current app and OS versions, and perhaps it works on earlier versions too.
+
+### License
+
+Copyright 2018 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ ShowHintsAlsoInOtherMasters.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,124 @@
+# encoding: utf-8
+
+############################################################################################
+#
+#
+#	Reporter Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Reporter
+#
+#
+############################################################################################
+
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+from AppKit import NSRect
+
+class ShowHintsAlsoInOtherMasters(ReporterPlugin):
+
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': u'PS Hints Also in Other Masters',
+			'de': u'PS-Hints auch in anderen Mastern',
+			'fr': u'hints PS aussi dans les autres masters',
+			'es': u'hints PS también en los otros masters'
+		})
+	
+	def rectifyRect(self, rect):
+		"""Converts rect with negative size into positive size."""
+		newRectOriginX = rect.origin[0]
+		newRectOriginY = rect.origin[1]
+		newRectSizeWidth = rect.size[0]
+		newRectSizeHeight = rect.size[1]
+		
+		if newRectSizeWidth < 0:
+			newRectSizeWidth *= -1
+			newRectOriginX -= newRectSizeWidth
+		if newRectSizeHeight < 0:
+			newRectSizeHeight *= -1
+			newRectOriginY -= newRectSizeHeight
+			
+		newRect = NSRect(
+			(newRectOriginX, newRectOriginY),
+			(newRectSizeWidth, newRectSizeHeight)
+		)
+		return newRect
+		
+	def background(self, layer):
+		# determine current master:
+		master = layer.associatedFontMaster()
+		
+		# determine hinted master:
+		glyph = layer.parent
+		if glyph:
+			font = glyph.parent
+			hintMasterID = font.customParameters["Get Hints From Master"]
+			if hintMasterID:
+				hintMaster = font.masters[hintMasterID]
+			else:
+				hintMaster = font.masters[0]
+			
+			if hintMaster != master or not Glyphs.defaults["showHints"]:
+				hintLayer = glyph.layers[hintMasterID]
+				layersCompatible = glyph.mastersCompatibleForLayers_((layer, hintLayer))
+				
+				if not layersCompatible:
+					pass # display a warning text
+				else:
+					if self.getScale() > 0.1:
+						# set drawing color
+						hColor = NSColor.colorWithRed_green_blue_alpha_(0.1, 0.5, 0.8, 0.2)
+						vColor = NSColor.colorWithRed_green_blue_alpha_(0.8, 0.5, 0.1, 0.2)
+						size = 10000
+				
+						for hint in hintLayer.hints:
+							if hint.type == TOPGHOST:
+								originNode = hint.originNode
+								originNodeIndex = originNode.index
+								originPathIndex = hintLayer.indexOfPath_(originNode.parent)
+								currentNode = layer.paths[originPathIndex].nodes[originNodeIndex]
+								if currentNode:
+									hColor.set()
+									drawRect = NSRect( (-size, currentNode.y-20), (size*2, 20) )
+									drawRect = self.rectifyRect(drawRect)
+									NSBezierPath.fillRect_( drawRect )
+						
+							elif hint.type == BOTTOMGHOST:
+								originNode = hint.originNode
+								originNodeIndex = originNode.index
+								originPathIndex = hintLayer.indexOfPath_(originNode.parent)
+								currentNode = layer.paths[originPathIndex].nodes[originNodeIndex]
+								if currentNode:
+									hColor.set()
+									drawRect = NSRect( (-size, currentNode.y), (size*2, 20) )
+									drawRect = self.rectifyRect(drawRect)
+									NSBezierPath.fillRect_( drawRect )
+						
+							elif hint.type == STEM:
+								originNode = hint.originNode
+								originNodeIndex = originNode.index
+								originPathIndex = hintLayer.indexOfPath_(originNode.parent)
+								currentOrigin = layer.paths[originPathIndex].nodes[originNodeIndex]
+						
+								targetNode = hint.targetNode
+								targetNodeIndex = targetNode.index
+								targetPathIndex = hintLayer.indexOfPath_(targetNode.parent)
+								currentTarget = layer.paths[targetPathIndex].nodes[targetNodeIndex]
+						
+								if currentOrigin and currentTarget:
+									if hint.horizontal:
+										hColor.set()
+										drawRect = NSRect( (-size, currentOrigin.y), (size*2, currentTarget.y-currentOrigin.y) )
+										drawRect = self.rectifyRect(drawRect)
+									else:
+										vColor.set()
+										drawRect = NSRect( (currentOrigin.x, -size), (currentTarget.x-currentOrigin.x, size*2) )
+										drawRect = self.rectifyRect(drawRect)
+										
+									NSBezierPath.fillRect_( drawRect )
+	
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
