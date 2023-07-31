be5f41414549ba0d179a9e5620b6fa3fa7533ac1
Rainer Scheichelbauer <res@glyphsapp.com>
2023-07-31 14:13:50 +0200

    Code cleanup

--- ShowHintsAlsoInOtherMasters.glyphsReporter/Contents/Resources/plugin.py
+++ ShowHintsAlsoInOtherMasters.glyphsReporter/Contents/Resources/plugin.py
@@ -18,20 +18,35 @@ from GlyphsApp.plugins import *
 from AppKit import NSRect
 
 class ShowHintsAlsoInOtherMasters(ReporterPlugin):
-
+	rgbH = 0.1, 0.5, 0.8
+	rgbV = 0.8, 0.5, 0.1
+	alphaDark = 0.6
+	alphaLight = 0.2
+	
+	prefID = "com.mekkablue.ShowHintsAlsoInOtherMasters"
+	
 	@objc.python_method
 	def settings(self):
 		self.menuName = Glyphs.localize({
-			'en': u'PS Hints Also in Other Masters',
-			'de': u'PS-Hints auch in anderen Mastern',
-			'fr': u'hints PS aussi dans les autres masters',
-			'es': u'hints PS también en los otros másteres',
+			'en': 'PS Hints Also in Other Masters',
+			'de': 'PS-Hints auch in anderen Mastern',
+			'fr': 'hints PS aussi dans les autres masters',
+			'es': 'hints PS también en los otros másteres',
 		})
 		
-		Glyphs.registerDefault("com.mekkablue.ShowHintsAlsoInOtherMasters.verticalStemHints", True)
-		Glyphs.registerDefault("com.mekkablue.ShowHintsAlsoInOtherMasters.horizontalStemHints", True)
-		Glyphs.registerDefault("com.mekkablue.ShowHintsAlsoInOtherMasters.ghostHints", True)
-		
+		Glyphs.registerDefault(self.domain("verticalStemHints"), True)
+		Glyphs.registerDefault(self.domain("horizontalStemHints"), True)
+		Glyphs.registerDefault(self.domain("ghostHints"), True)
+	
+	@objc.python_method
+	def domain(self, prefName):
+		prefName = prefName.strip().strip(".")
+		return self.prefID + "." + prefName.strip()
+	
+	@objc.python_method
+	def pref(self, prefName):
+		prefDomain = self.domain(prefName)
+		return Glyphs.defaults[prefDomain]
 	
 	@objc.python_method
 	def rectifyRect(self, rect):
@@ -62,7 +77,7 @@ class ShowHintsAlsoInOtherMasters(ReporterPlugin):
 			if font:
 				windowController = font.parent.windowController()
 				if not windowController.SpaceKey():
-					self.drawHints( layer )
+					self.drawHints(layer)
 	
 	@objc.python_method
 	def preview(self, layer):
@@ -70,26 +85,30 @@ class ShowHintsAlsoInOtherMasters(ReporterPlugin):
 			# white background:
 			self.drawHints(
 				layer, size=20, vsize=5000,
-				# hColor = NSColor.colorWithRed_green_blue_alpha_(0.1, 0.5, 0.8, 0.2), 
-				# vColor = NSColor.colorWithRed_green_blue_alpha_(0.8, 0.5, 0.1, 0.2), 
 				inPreview = True,
 			)
 		else:
 			# black background:
 			self.drawHints(
 				layer, size=20, vsize=5000,
-				hColor = NSColor.colorWithRed_green_blue_alpha_(0.1, 0.5, 0.8, 0.6),
-				vColor = NSColor.colorWithRed_green_blue_alpha_(0.8, 0.5, 0.1, 0.6),
+				hColor = NSColor.colorWithRed_green_blue_alpha_(*(self.rgbH), self.alphaDark),
+				vColor = NSColor.colorWithRed_green_blue_alpha_(*(self.rgbV), self.alphaDark),
 				inPreview = True,
 			)
 	
 	@objc.python_method
 	def drawHints(
 				self, layer, size=10000, vsize=0,
-				hColor = NSColor.colorWithRed_green_blue_alpha_(0.1, 0.5, 0.8, 0.2), 
-				vColor = NSColor.colorWithRed_green_blue_alpha_(0.8, 0.5, 0.1, 0.2), 
+				hColor = None, 
+				vColor = None, 
 				inPreview = False,
 			):
+		
+		# fallback colors:
+		if not hColor:
+			hColor = NSColor.colorWithRed_green_blue_alpha_(*self.rgbH, self.alphaLight)
+		if not vColor:
+			vColor = NSColor.colorWithRed_green_blue_alpha_(*self.rgbV, self.alphaLight)
 		
 		glyph = layer.parent
 		if inPreview:
@@ -120,125 +139,124 @@ class ShowHintsAlsoInOtherMasters(ReporterPlugin):
 				hintLayer = layer
 				layersCompatible = True
 				
-			if layersCompatible:
-				if self.getScale() > 0.05:
-					bboxLeft = layer.bounds.origin.x
-					bboxBottom = layer.bounds.origin.y
-					bboxHeight = layer.bounds.size.height
-					bboxWidth = layer.bounds.size.width
-					
-					for hint in hintLayer.hints:
-						if hint.type == TOPGHOST and Glyphs.defaults[ "com.mekkablue.ShowHintsAlsoInOtherMasters.ghostHints" ]:
-							originNode = hint.originNode
-							if originNode:
-								if inPreview:
-									currentNode = originNode
-								else:
-									originNodeIndex = originNode.index
+			if layersCompatible and self.getScale() > 0.05:
+				bboxLeft = layer.bounds.origin.x
+				bboxBottom = layer.bounds.origin.y
+				bboxHeight = layer.bounds.size.height
+				bboxWidth = layer.bounds.size.width
+				
+				for hint in hintLayer.hints:
+					if hint.type == TOPGHOST and self.pref("ghostHints"):
+						originNode = hint.originNode
+						if originNode:
+							if inPreview:
+								currentNode = originNode
+							else:
+								originNodeIndex = originNode.index
+								originPathIndex = hintLayer.indexOfPath_(originNode.parent)
+								currentNode = layer.paths[originPathIndex].nodes[originNodeIndex]
+							if currentNode:
+								hColor.set()
+								drawRect = NSRect((bboxLeft-size, currentNode.y-20), (bboxWidth+size*2, 20))
+								drawRect = self.rectifyRect(drawRect)
+								NSBezierPath.fillRect_(drawRect)
+				
+					elif hint.type == BOTTOMGHOST and self.pref("ghostHints"):
+						originNode = hint.originNode
+						if originNode:
+							if inPreview:
+								currentNode = originNode
+							else:
+								originNodeIndex = originNode.index
+								try:
+									originPathIndex = hintLayer.indexOfObjectInShapes_(originNode.parent)
+									currentNode = layer.shapes[originPathIndex].nodes[originNodeIndex]
+								except:
 									originPathIndex = hintLayer.indexOfPath_(originNode.parent)
 									currentNode = layer.paths[originPathIndex].nodes[originNodeIndex]
-								if currentNode:
+							if currentNode:
+								hColor.set()
+								drawRect = NSRect((bboxLeft-size, currentNode.y), (bboxWidth+size*2, 20))
+								drawRect = self.rectifyRect(drawRect)
+								NSBezierPath.fillRect_(drawRect)
+				
+					elif hint.type == STEM:
+						originNode = hint.originNode
+						targetNode = hint.targetNode
+						
+						if originNode and targetNode:
+							if inPreview:
+								currentOrigin = originNode
+								currentTarget = targetNode
+							else:
+								# origin:
+								originNodeIndex = originNode.index
+								try:
+									originPathIndex = hintLayer.indexOfObjectInShapes_(originNode.parent)
+									currentOrigin = layer.shapes[originPathIndex].nodes[originNodeIndex]
+								except:
+									originPathIndex = hintLayer.indexOfPath_(originNode.parent)
+									currentOrigin = layer.paths[originPathIndex].nodes[originNodeIndex]
+								# target:
+								targetNodeIndex = targetNode.index
+								try:
+									targetPathIndex = hintLayer.indexOfObjectInShapes_(targetNode.parent)
+									currentTarget = layer.shapes[targetPathIndex].nodes[targetNodeIndex]
+								except:
+									targetPathIndex = hintLayer.indexOfPath_(targetNode.parent)
+									currentTarget = layer.paths[targetPathIndex].nodes[targetNodeIndex]
+							if currentOrigin and currentTarget:
+								if hint.horizontal and self.pref("horizontalStemHints"):
 									hColor.set()
-									drawRect = NSRect( (bboxLeft-size, currentNode.y-20), (bboxWidth+size*2, 20) )
+									drawRect = NSRect((bboxLeft-size, currentOrigin.y), (bboxWidth+size*2, currentTarget.y-currentOrigin.y))
 									drawRect = self.rectifyRect(drawRect)
-									NSBezierPath.fillRect_( drawRect )
-					
-						elif hint.type == BOTTOMGHOST and Glyphs.defaults[ "com.mekkablue.ShowHintsAlsoInOtherMasters.ghostHints" ]:
-							originNode = hint.originNode
-							if originNode:
-								if inPreview:
-									currentNode = originNode
-								else:
-									originNodeIndex = originNode.index
-									try:
-										originPathIndex = hintLayer.indexOfObjectInShapes_(originNode.parent)
-										currentNode = layer.shapes[originPathIndex].nodes[originNodeIndex]
-									except:
-										originPathIndex = hintLayer.indexOfPath_(originNode.parent)
-										currentNode = layer.paths[originPathIndex].nodes[originNodeIndex]
-								if currentNode:
-									hColor.set()
-									drawRect = NSRect( (bboxLeft-size, currentNode.y), (bboxWidth+size*2, 20) )
+									NSBezierPath.fillRect_(drawRect)
+								elif not hint.horizontal and self.pref("verticalStemHints"):
+									vColor.set()
+									drawRect = NSRect((currentOrigin.x, bboxBottom-size-vsize), (currentTarget.x-currentOrigin.x, bboxHeight+(size+vsize)*2))
 									drawRect = self.rectifyRect(drawRect)
-									NSBezierPath.fillRect_( drawRect )
-					
-						elif hint.type == STEM:
-							originNode = hint.originNode
-							targetNode = hint.targetNode
-							
-							if originNode and targetNode:
-								if inPreview:
-									currentOrigin = originNode
-									currentTarget = targetNode
-								else:
-									# origin:
-									originNodeIndex = originNode.index
-									try:
-										originPathIndex = hintLayer.indexOfObjectInShapes_(originNode.parent)
-										currentOrigin = layer.shapes[originPathIndex].nodes[originNodeIndex]
-									except:
-										originPathIndex = hintLayer.indexOfPath_(originNode.parent)
-										currentOrigin = layer.paths[originPathIndex].nodes[originNodeIndex]
-									# target:
-									targetNodeIndex = targetNode.index
-									try:
-										targetPathIndex = hintLayer.indexOfObjectInShapes_(targetNode.parent)
-										currentTarget = layer.shapes[targetPathIndex].nodes[targetNodeIndex]
-									except:
-										targetPathIndex = hintLayer.indexOfPath_(targetNode.parent)
-										currentTarget = layer.paths[targetPathIndex].nodes[targetNodeIndex]
-								if currentOrigin and currentTarget:
-									if hint.horizontal and Glyphs.defaults[ "com.mekkablue.ShowHintsAlsoInOtherMasters.horizontalStemHints" ]:
-										hColor.set()
-										drawRect = NSRect( (bboxLeft-size, currentOrigin.y), (bboxWidth+size*2, currentTarget.y-currentOrigin.y) )
-										drawRect = self.rectifyRect(drawRect)
-										NSBezierPath.fillRect_( drawRect )
-									elif not hint.horizontal and Glyphs.defaults[ "com.mekkablue.ShowHintsAlsoInOtherMasters.verticalStemHints" ]:
-										vColor.set()
-										drawRect = NSRect( (currentOrigin.x, bboxBottom-size-vsize), (currentTarget.x-currentOrigin.x, bboxHeight+(size+vsize)*2) )
-										drawRect = self.rectifyRect(drawRect)
-										NSBezierPath.fillRect_( drawRect )
+									NSBezierPath.fillRect_(drawRect)
 	
 	def conditionalContextMenus(self):
 		return [
 		{
 			'name': Glyphs.localize({
-				'en': u"‘Show PS Hints’ Options:", 
-				'de': u"Einstellungen für »PS-Hints anzeigen«:", 
-				'es': u"Opciones para ‘Mostrar hints PS’:", 
-				'fr': u"Options pour «Afficher les hints PS»:",
+				'en': "‘Show PS Hints’ Options:", 
+				'de': "Einstellungen für »PS-Hints anzeigen«:", 
+				'es': "Opciones para ‘Mostrar hints PS’:", 
+				'fr': "Options pour «Afficher les hints PS»:",
 				}), 
 			'action': None,
 		},
 		{
 			'name': Glyphs.localize({
-				'en': u"Show Vertical Stem Hints",
-				'de': u"Senkrechte Stamm-Hints anzeigen",
-				'es': u"Mostrar hints verticales",
-				'fr': u"Afficher hints verticaux",
+				'en': "Show Vertical Stem Hints",
+				'de': "Senkrechte Stamm-Hints anzeigen",
+				'es': "Mostrar hints verticales",
+				'fr': "Afficher hints verticaux",
 				}), 
 			'action': self.toggleVerticalStemHints,
-			'state': Glyphs.defaults[ "com.mekkablue.ShowHintsAlsoInOtherMasters.verticalStemHints" ],
+			'state': self.pref("verticalStemHints"),
 		},
 		{
 			'name': Glyphs.localize({
-				'en': u"Show Horizontal Stem Hints",
-				'de': u"Waagrechte Stamm-Hints anzeigen",
-				'es': u"Mostrar hints horizontales",
-				'fr': u"Afficher hints horizontaux",
+				'en': "Show Horizontal Stem Hints",
+				'de': "Waagrechte Stamm-Hints anzeigen",
+				'es': "Mostrar hints horizontales",
+				'fr': "Afficher hints horizontaux",
 				}), 
 			'action': self.toggleHorizontalStemHints,
-			'state': Glyphs.defaults[ "com.mekkablue.ShowHintsAlsoInOtherMasters.horizontalStemHints" ],
+			'state': self.pref("horizontalStemHints"),
 		},
 		{
 			'name': Glyphs.localize({
-				'en': u"Show Ghost Hints",
-				'de': u"Ghost-Hints anzeigen",
-				'es': u"Mostrar ghost hints",
-				'fr': u"Afficher hints «ghost»",
+				'en': "Show Ghost Hints",
+				'de': "Ghost-Hints anzeigen",
+				'es': "Mostrar ghost hints",
+				'fr': "Afficher hints «ghost»",
 				}), 
 			'action': self.toggleGhostHints,
-			'state': Glyphs.defaults[ "com.mekkablue.ShowHintsAlsoInOtherMasters.ghostHints" ],
+			'state': self.pref("ghostHints"),
 		},
 		]
 
@@ -253,8 +271,8 @@ class ShowHintsAlsoInOtherMasters(ReporterPlugin):
 	
 	@objc.python_method
 	def toggleSetting(self, prefName):
-		pref = "com.mekkablue.ShowHintsAlsoInOtherMasters.%s" % prefName
-		Glyphs.defaults[pref] = not bool(Glyphs.defaults[pref])
+		domain = self.domain(prefName)
+		Glyphs.defaults[domain] = not bool(self.pref(prefName))
 	
 	def addMenuItemsForEvent_toMenu_(self, event, contextMenu):
 		if self.generalContextMenus:
